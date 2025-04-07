a21edc319e0cac677b45667954f4e7a23fc68c39
spamuel <doylsamu@hotmail.com>
2025-04-07 19:08:57 -0400

    electricpoultry

--- src/main/java/io/github/coolman4567/xboxmanlib/util/Basic.java
+++
@@ -1,7 +0,0 @@
-package io.github.coolman4567.xboxmanlib.util;
-
-public class Basic {
-    public static String prefixNamespace(ModResourceLocation registryKey) {
-        return registryKey.getNamespace().equals("minecraft") ? registryKey.getPath() : registryKey.getNamespace() + "/" + registryKey.getPath();
-    }
-}
---
+++ src/main/java/io/github/electricpoultry/electricpoultrylib/item/ModItems.java
@@ -0,0 +1,5 @@
+package io.github.electricpoultry.electricpoultrylib.item;
+
+public class ModItems {
+
+}
